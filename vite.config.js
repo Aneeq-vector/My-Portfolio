1d32ad80ec3f069d45401246c53097958e2393bd
Ahmed Aneeq <ahmedaneeq.official@gmail.com>
2026-05-07 21:21:20 +0530

    Update  vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    // Reduce chunk size
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Vendor libraries (~200KB)
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          // Other deps
+          'axios': ['axios'],
+          'motion': ['motion']
+        }
+      }
+    }
+  }
 })
